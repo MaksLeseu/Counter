3e25208c48f50064d996dd0f51e068f3de7409e6
Maxim Lesev <lesov0iblr@gmail.com>
2023-06-27 20:41:17 +0200

    refactored code

--- src/common/Theme/GlobalTheme.tsx
+++ src/common/Theme/GlobalTheme.tsx
@@ -1,6 +1,5 @@
-import {createTheme, styled, ThemeProvider} from "@mui/material";
+import {createTheme, ThemeProvider} from "@mui/material";
 import React, {ReactNode} from "react";
-import Box from '@mui/material/Box';
 import {useSelector} from "react-redux";
 import {colorModeSelector} from "../../state/selectors/darkMode-selectors";
 
@@ -52,55 +51,3 @@ export const GlobalTheme: React.FC<ThemeType> = ({children}) => {
 
 export const LIGHT_MODE: string = 'Light mode';
 export const DARK_MODE: string = 'Dark mode';
-
-export const BoxCustomized = styled(Box)(
-    ({theme}) => theme.palette.mode === 'dark'
-        ?
-            `
-                background-color: indigo;
-                border: 2px white solid;
-                box-shadow: 1px 3px 10px white;
-            `
-        :
-            `
-                background-color: #DCDCDC;
-                border: 2px black solid;
-                box-shadow: 1px 3px 10px black;
-            `
-);
-
-/*
-width: 350px;
-height: 250px;
-border-radius: 5px;
-margin: 0 auto;*/
-
-export const DisplayCustomized = styled(Box)(
-    ({theme}) => theme.palette.mode === 'dark'
-        ?
-            `
-                border: 1px white solid;
-                color: white;
-            `
-        :
-            `
-                border: 1px black solid;
-                color: black;
-            `
-);
-
-export const SettingsCustomized = styled(Box)(
-    ({theme}) => theme.palette.mode === 'dark'
-        ?
-            `
-                border: 2px white solid;
-                background-color: indigo;
-                box-shadow: 1px 3px 10px white;
-            `
-        :
-            `
-                border: 2px black solid;
-                background-color: #DCDCDC;
-                box-shadow: 1px 3px 10px black;
-            `
-);
